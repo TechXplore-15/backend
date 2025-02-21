a94b7897adc2cb4f7db58e5b61b187d5c5022ba3
zhvanka <zhvania@ted.ge>
2025-02-21 23:25:40 +0400

    Fix: Deployment to Live

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Python image
+FROM python:3.12-bookworm
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file into the container
+COPY requirements.txt .
+
+# Install Deps
+RUN pip install djangorestframework markdown django-filter gunicorn
+
+# Run manage.py on serverside
+#CMD ["python", "manage.py", "runserver"]
+
+# Run Gunicoooorn
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "djangoProject.wsgi"]
+
+# Expose Python Runserver Port
+EXPOSE 8000
--- cardcontrol/views.py
+++ cardcontrol/views.py
@@ -1,21 +1,63 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
-from cardcontrol.serializers import *
-from cardcontrol.models import *
+from .models import Card
+from .serializers import CardSerializer
+from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
 
 
-
-# Create your views here.
-
-
 class CardViewset(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     serializer_class = CardSerializer
+    queryset = Card.objects.all()
 
-    def get_queryset(self):
-        user_id_received = self.request.query_params["user_id"]
+    def create(self, request, *args, **kwargs):
+        user_id = request.data.get("user")
+        if not user_id:
+            return Response({"error": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        print(user_id_received)
-        all_cards = Card.objects.filter(user__id=user_id_received)
-        return all_cards
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, *args, **kwargs):
+        card_id = request.data.get("card_id")
+        if not card_id:
+            return Response({"error": "Card ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            card = Card.objects.get(id=card_id)
+        except Card.DoesNotExist:
+            return Response({"error": "Card not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        user_id = request.data.get("user")
+        if user_id:
+            try:
+                user = User.objects.get(id=user_id)
+                card.user = user
+            except User.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        if "card_name" in request.data:
+            card.card_name = request.data["card_name"]
+
+        if "card_account" in request.data:
+            card.card_account = request.data["card_account"]
+
+        if "end_date" in request.data:
+            card.end_date = request.data["end_date"]
+
+        if "is_active" in request.data:
+            card.is_active = request.data["is_active"]
+        card.save()
+        serializer = self.get_serializer(card)
+        return Response(serializer.data, status=status.HTTP_200_OK)
