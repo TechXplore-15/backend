5b7294e2e8cfc4d6de67ce41f15813fb05b44cb8
ikakopa1985 <ikakopa1985@gmail.com>
2025-02-22 21:37:19 +0400

    26

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -86,11 +87,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 
 
 # Password validation
